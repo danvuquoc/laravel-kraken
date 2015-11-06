bf3d107cb5a908bf1fbf0396e467069b1c8a2092
Dan Vu Quoc <dan@vuquoc.com>
2015-11-06 10:06:59 -0800

    Initial commit

---
+++ config/kraken.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Kraken API Credentials
+    |--------------------------------------------------------------------------
+    |
+    | Your Kraken API Credentials. If you don't know this information, find it here:
+    | "https://kraken.io/account/api-credentials"
+    |
+    */
+    'api_key' => 'kraken-api-key',
+    'api_secret' => 'kraken-api-secret',
+];
---
+++ src/KrakenServiceProvider.php
@@ -0,0 +1,37 @@
+<?php namespace Danvuquoc\Kraken;
+
+use Illuminate\Support\ServiceProvider;
+use Kraken;
+
+class KrakenServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Set up the publishing of configuration
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/../config/kraken.php' => config_path('kraken.php')
+        ]);
+    }
+
+    /**
+     * Register the Kraken instance with the api authentication information.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('Kraken', function($app) {
+            return new Kraken($app['config']['kraken']['api_key'], $app['config']['kraken']['api_secret']);
+        });
+    }
+}
